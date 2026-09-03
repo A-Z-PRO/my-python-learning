8ea87a62f93fe59e184700ca488a12f4a8972f41
A-Z-PRO <3498952074@qq.com>
2026-09-04 04:27:43 +0800

    Add files via upload

---
+++ lesson1_variables.py
@@ -0,0 +1,14 @@
+# 这是我的第一个变量练习
+name = "xy"
+age = 20
+height = 1.75
+is_learning_python = True
+
+print("姓名：", name)
+print("年龄：", age)
+print("身高：", height, "米")
+print("正在学Python：", is_learning_python)
+
+# 计算明年年龄
+next_year_age = age + 1
+print("明年我", next_year_age, "岁")
